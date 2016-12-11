64814d1c85b424beef7e4007402dcd30c7a3562b
LI ZEYAN <zy-li14@hotmail.com>
2016-12-11 11:22:24 +0800

    Can run

--- run_srcnn.py
+++ run_srcnn.py
@@ -9,18 +9,18 @@ from Network import *
 
 train_data, test_data, train_label, test_label = load_data()
 model = Network()
-model.add(Convolution('conv1', 7, 3, 8, 0.01))
+model.add(Convolution('conv1', 9, 3, 64, 0.01))
 model.add(ReLU('relu1'))
-model.add(Convolution('conv2', 5, 8, 16, 0.01))
+model.add(Convolution('conv2', 1, 64, 32, 0.01))
 model.add(ReLU('relu2'))
-model.add(Convolution('conv3', 3, 16, 3, 0.01))
-model.add(ReLU('relu3'))
+model.add(Convolution('conv3', 5, 32, 3, 0.01))
 
 loss = MSELoss('MSELoss')
 optimizer = tf.train.AdamOptimizer(1e-4)
 input_placeholder = tf.placeholder(tf.float32)
 label_placeholder = tf.placeholder(tf.float32)
 model.compile(input_placeholder, label_placeholder, loss, optimizer)
-solve_net(model, train_data, train_label, test_data, test_label, batch_size=32, max_epoch=100, disp_freq=100, test_freq=1000)
+solve_net(model, train_data, train_label, test_data, test_label,
+          batch_size=32, max_epoch=10000, disp_freq=100, test_freq=1000)
 
 
